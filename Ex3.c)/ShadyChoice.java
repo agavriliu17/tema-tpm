d5c2bf9033dd1fc232799f7ab91261301bfbb02e
HaidauRobert <79244860+HaidauRobert@users.noreply.github.com>
2022-11-10 11:16:19 +0200

    Update ShadyChoice.java

--- Ex3.c)/ShadyChoice.java
+++ Ex3.c)/ShadyChoice.java
@@ -18,11 +18,12 @@ class ShadyChoice {
 
 /*
 Putem observa ca functia choose() este apelata de toate threadurile. Primele threaduri vor ajunge la instructiunea
-if getWhite cand getWhite are valoarea false deci vor trece mai departe si vor seta getWhite pe true. Dupa aceea
-threadurile vor ajunge la if-ul care verifica daca last este egal cu me. Daca threadurile ajung la acest if in
-aceeasi ordine in care au ajuns la if-ul anterior, atunci doar ultimul thread care a trecut de if-ul anterior va
-returna "red" deoarece valoarea lui last ramane a aceluia, iar threadurile dinainte de acesta vor returna "black".
-In concluzie, un singur thread poate avea valoarea "red", iar daca acest thread este si ultimul thread vom avea restul
+if getWhite (linia 8) cand getWhite are valoarea false deci vor trece mai departe si vor seta getWhite pe true. Dupa aceea
+threadurile vor ajunge la if-ul care verifica daca last este egal cu me (linia 11). Daca threadurile ajung la acest if (linia 11)
+in aceeasi ordine in care au ajuns la if-ul anterior, atunci doar ultimul thread care a trecut de if-ul anterior va
+returna "red" deoarece valoarea lui last ramane a aceluia in caz ca valoarea lui last nu a fost schimbata, in acest caz
+nu vom avea nici un thread care sa returneze "red", iar threadurile dinainte de acesta vor returna "black".
+In concluzie, maxim un singur thread poate avea valoarea "red", iar daca acest thread este si ultimul thread vom avea restul
 de n-1 threaduri cu valoarea "black", deci putem avea maxim un thread cu valoarea "red" si maxim n-1 threaduri cu valoarea
 black.
 */
